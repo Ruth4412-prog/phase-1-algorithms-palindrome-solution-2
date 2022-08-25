dc8ebc8fc855f2388827a0c5e9dc7aef450a9f8d
Ruth Jepkalal <kipchauijepkaa@gmail.com>
2022-08-25 05:10:25 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  //Write your algorithm here
+  for (let m = 0; m < word.length / 2; m++) {
+    const n = word.length - 1 - m;
+    if (word[m] !== word[n]) {
+      return false;
+    }
+  }
+
+  return true;
 }
+
 
 /* 
   Add your pseudocode here
+  write a function tat retursn true if the word is a palidrome
+  if the first and last letters  are the same and if the second letter and the second last letter are the same 
+  and so on it should return true else should return false.
 */
 
 /*
   Add written explanation of your solution here
+  It should return if the same and false if not.
+  iterate from the beginning to the middle when comparing
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +34,12 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ab"));
+
 }
 
-module.exports = isPalindrome;
+    module.exports = isPalindrome;
